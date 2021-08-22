3f6490720bd0124468e4b33ad611e272b1a386df
AndresPMD <andresmafladelgado@gmail.com>
2021-08-22 14:11:39 +0200

    added cnns

--- evaluate_models.py
+++ evaluate_models.py
@@ -7,7 +7,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_name', default='f30k', help="{coco, f30k}")
-    parser.add_argument('--cnn', default='ViT-B/16')
+    parser.add_argument('--cnn', default='RN50x16', help='[RN50, RN101, RN50x4, RN50x16, ViT-B/32, ViT-B/16]')
     parser.add_argument('--split', default='test', help='Split, test(1K), testall(5k) (Coco only)')
     parser.add_argument('--batch_size', default=1, type=int)
     parser.add_argument('--workers', default=8, type=int)
